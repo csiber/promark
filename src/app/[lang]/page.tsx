8752e26c04d0c84a5f42a652faf3481ce9a5b1ab
Polyák Csaba <csiberius@gmail.com>
2024-06-10 12:08:16 +0000

    Remove `export const runtime = 'edge';` from three page components.

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,5 +1,5 @@
 import PageClientLayout_Home from "./_components/home";
-export const runtime = 'edge';
+
 export default async function Home({ params }: { params: { lang: string } }) {
   // const lang = params.lang;
   return <PageClientLayout_Home />;
