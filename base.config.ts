0604070677f31387cad8286ced563bfb7281d701
Polyák Csaba <csiberius@gmail.com>
2024-06-10 11:01:45 +0000

    first commit

---
+++ base.config.ts
@@ -0,0 +1,127 @@
+import { Config } from "@/types/_config";
+import { getURL } from "@/utils/main";
+
+// Base Configuration
+const _config: Config = {
+  year: "2024",
+  // Author
+  author: {
+    name: "PromNET",
+    url: "https://promnet.cloud/",
+    socialAccounts: {
+      email: "info@promnet.cloud",
+      github: "https://github.com/csiber",
+      discord: "https://discord.com/channels/917759709468037190/917759709468037193",
+    },
+  },
+  // Metadata
+  meta: {
+    origin: "https://promark.promnet.cloud", // Site origin
+    title: "PromARK Game Servers", // Site title
+    description:
+      "Welcome to PromARK Game Servers! We offer comprehensive ARK server hosting, ensuring all your favorite servers are just a click away. Enjoy stable, fast, and reliable gameplay with our top-tier services. Join us and elevate your ARK experience!", // Site description
+    authors: {
+      name: "PromNET", // Author's name
+      url: "https://promnet.cloud/", // Author's URL
+    },
+    Image: { icon: "/favicon.ico", apple: "/favicon.png" }, // Icon images
+    cardImage:
+      "https://promark.promnet.cloud/img/screenshot/en-light-fullscreen.png", // Card image
+    robots: "follow, index", // Robots meta tag content
+    url: getURL(), // Site URL
+  },
+  // i18n Configuration
+  i18n: {
+    namespaces: [
+      "translation", // Translation file namespaces
+      "site", // Site-specific translation file namespaces
+    ],
+    defaultLocale: "en", // Default language
+    locales: [
+      "ja", // Japanese
+      "en", // English
+      "hu", // Magyar
+    ],
+    path: "i18n", // Path to translation files
+    localeConfigs: {
+      ja: {
+        label: "日本語", // Japanese label
+        htmlLang: "ja-JP", // HTML lang attribute value
+        path: "ja", // Path
+      },
+      en: {
+        label: "English", // English label
+        htmlLang: "en-US", // HTML lang attribute value
+        path: "en", // Path
+      },
+      hu: {
+        label: "Magyar", // Hungarian label
+        htmlLang: "hu-HU", // HTML lang attribute value
+        path: "hu", // Path
+      },
+    },
+  },
+  // Sitemap Configuration
+  sitemap: {
+    excludedDirs: [
+      "error", // Directory for error pages
+      "not-found", // Directory for 404 pages
+    ],
+  },
+  navigation: {
+    ui: {
+      home_url: "/",
+      logo: {
+        url: "/brand/promnet/arklogo.png",
+        url_dark: '/brand/promnet/arklogo.png',
+        alt: "PromARK Logo",
+      },
+      label: "PromARK Game Servers",
+      style: {
+        bg: "none",
+      },
+    },
+    contents: [
+      {
+        type: "one",
+        label: "Home",
+        url: "/",
+      },
+      {
+        type: "one",
+        label: "Discord",
+        url: "https://discord.gg/QumXfEs5",
+        target: "_blank",
+      },
+      {
+        type: "one",
+        label: "Hosting",
+        url: "https://promnet.cloud",
+        target: "_blank",
+      },
+    ],
+  },
+  footer: {
+    ui: {
+      label: "PromNET Hosting",
+      logo: {
+        url: "/brand/promnet/cloud.webp",
+        alt: "PromNET Logo",
+        css: "border overflow-hidden rounded-full shadow-sm",
+      },
+      style: {
+        bg: "primary",
+        dataBsTheme: "dark",
+      },
+    },
+    contents: [
+
+
+    ],
+  },
+};
+
+// If you update the languages in i18n, you need to manually update the config in src/middleware.ts.
+// (If the locales array in i18n is changed, you need to update the matcher section in middleware.ts)
+
+export default _config;
---
+++ src/app/[lang]/_components/home.tsx
@@ -0,0 +1,137 @@
+"use client";
+
+// Layout
+import BasePageLayout from "@/components/client/base_layout";
+
+// React
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+
+// React bootstrap
+import {
+  Accordion,
+  Badge,
+  Button,
+  Card,
+  Col,
+  Container,
+  Form,
+  Nav,
+  NavDropdown,
+  Navbar,
+  ProgressBar,
+  Row,
+} from "react-bootstrap";
+
+// i18n
+import { useLanguage, useTranslation } from "@/app/i18n/client";
+
+const modules_card = [
+  {
+    title: "Primal Paradise",
+    description: "Map: The Island, Rates: 3x Harvesting, 3x Taming, 3x XP",
+    url: "steam://gs.promnet.cloud:7777",
+    image_url: "/brand/servers/the_island_card.webp",
+    version: `PVE`,
+  },
+  {
+    title: "Survival Sanctuary",
+    description: "Map: The Center, Rates: 3x Harvesting, 3x Taming, 5x XP",
+    url: "steam://gs.promnet.cloud:7778",
+    image_url: "/brand/servers/the_center_card.avif",
+    version: `PVP`,
+  },
+  {
+    title: "Dino Dominion",
+    description:
+      "Map: Ragnarok, Rates: 4x Harvesting, 2.5x Taming, 2x XP",
+    url: "steam://gs.promnet.cloud:7779",
+    image_url: "/brand/servers/ragnarok_card.webp",
+    version: `PVP`,
+  },
+  {
+    title: "Jurassic Journey",
+    description:
+      "Map: Valguero, Rates: 4x Harvesting, 3x Taming, 2x XP",
+    url: "steam://gs.promnet.cloud:7780",
+    image_url: "/brand/servers/valguero_card.webp",
+    version: `PVP`,
+  },
+  {
+    title: "Eternal Eden",
+    description:
+      "Map: Crystal Isles, Rates: 5x Harvesting, 4x Taming, 3x XP",
+    url: "steam://gs.promnet.cloud:7781",
+    image_url: "/brand/servers/crystal_isles_card.webp",
+    version: `PVP`,
+  },
+  {
+    title: "Ancient Ark",
+    description:
+      "Map: Scorched Earth, Rates: 3x Harvesting, 2x Taming, 1.5x XP",
+    url: "steam://gs.promnet.cloud:7782",
+    image_url: "/brand/servers/scorched_earth_card.webp",
+    version: `PVP`,
+  },
+  {
+    title: "Wild Wilderness",
+    description:
+      "Map: Extinction, Rates: 4x Harvesting, 3x Taming, 2x XP",
+    url: "steam://gs.promnet.cloud:7783",
+    image_url: "/brand/servers/extinction_card.webp",
+    version: `PVP`,
+  },
+  {
+    title: "Genesis Grounds",
+    description:
+      "Map: Genesis Part 1, Rates: 5x Harvesting, 4x Taming, 3x XP",
+    url: "steam://gs.promnet.cloud:7784",
+    image_url: "/brand/servers/genesispart1_card.webp",
+    version: `PVP`,
+  },
+  {
+    title: "Dino Dynasty",
+    description:
+      "Map: Aberration, Rates: 3x Harvesting, 2x Taming, 1.5x XP",
+    url: "steam://gs.promnet.cloud:7785",
+    image_url: "/brand/servers/aberration_card.webp",
+    version: `PVP`,
+  },
+];
+
+export default function PageClientLayout_Home() {
+  const { language } = useLanguage();
+  const { t } = useTranslation(language);
+
+  return (
+    <BasePageLayout>
+      <div className="flex flex-col gap-2 text-center py-5">
+        <h1 className="font-bold text-5xl">{t("Next.js base template")}</h1>
+        <p>
+          {t(
+            "We're excited to present our lineup of ARK servers, ready for you to explore and conquer! Below are the details of each server."
+          )}
+        </p>
+      </div>
+      <Row xs={1} md={2} className="g-4">
+        {modules_card.map((card, idx) => (
+          <Col key={idx}>
+            <Link target="_block" href={card.url} className="no-underline">
+              <Card>
+                <div
+                  className="w-full h-[260px] bg-cover bg-no-repeat bg-center rounded-[0.375rem] rounded-b-none"
+                  style={{ backgroundImage: `url(${card.image_url})` }}
+                />
+                <Card.Body>
+                  <Card.Title>{card.title}</Card.Title>
+                  <Card.Text>{card.description}</Card.Text>
+                  <Badge bg="primary">{card.version}</Badge>
+                </Card.Body>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </BasePageLayout>
+  );
+}
