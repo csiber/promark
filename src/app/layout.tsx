b95e892befa77fe5e3a6ebc891564c5d5d3eebfb
Polyák Csaba <csiberius@gmail.com>
2024-06-10 12:24:54 +0000

    "Added @vercel/speed-insights dependency and imported SpeedInsights component in layout.tsx"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 // next-themes
 import { ThemeProvider } from "next-themes";
 import { Suspense } from "react";
